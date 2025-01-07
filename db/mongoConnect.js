e18e2019e643b92af122458da3eb87901a2c77b3
shimon driham <95618903+shimondriham@users.noreply.github.com>
2025-01-07 20:17:51 +0200

    a

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-// main().catch((err) => console.log(err));
+main().catch((err) => console.log(err));
 
-// async function main() {
-//   mongoose.set("strictQuery", false);
-//   await mongoose.connect(
-//     // `mongodb+srv://re_cv_server:1234@cluster0.vruq0.mongodb.net/play_cv`
-//     `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.vruq0.mongodb.net/play_cv`
-//   );
-//   console.log("mongo connect");
-// }
+async function main() {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(
+    // `mongodb+srv://re_cv_server:1234@cluster0.vruq0.mongodb.net/play_cv`
+    `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.vruq0.mongodb.net/play_cv`
+  );
+  console.log("mongo connect");
+}
